32798ee77f12833565f970f3d077f6f5adb4c7b3
Siyeon Won <96099990+1unasy@users.noreply.github.com>
2024-04-28 16:38:50 +0900

    [Bronze III] Title: 최소, 최대, Time: 384 ms, Memory: 153848 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -0,0 +1,7 @@
+n = int(input())
+num = list(map(int, input().split()))
+if n != len(num):
+    compared = '적습니다' if len(num) < n else '많습니다'
+    print(f'입력 숫자 개수가 {n}개 보다 {compared}. 다시 입력해주세요.')
+    num = list(map(int, input().split()))
+print(min(num), max(num))  
