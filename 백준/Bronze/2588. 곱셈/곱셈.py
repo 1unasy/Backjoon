e15d6e3e58887bbdd74d55b7e341e016d9592d92
Siyeon Won <96099990+1unasy@users.noreply.github.com>
2024-04-28 13:15:24 +0900

    Update 곱셈.py

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -5,3 +5,7 @@ splited_b = [int(x) for x in str(b)][::-1]
 for x in splited_b:
     print(a * x, end='\n')
 print(a*b)
+
+# splited_b = [a * int(x) for x in str(b)][::-1]
+# print(splited_b[0], splited_b[1], splited_b[2], sep='\n')
+# print(a*b)
