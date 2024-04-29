787f24f804ab6a0b9014cefed4d4e28aa27b668f
Siyeon Won <96099990+1unasy@users.noreply.github.com>
2024-04-29 18:12:33 +0900

    [Bronze I] Title: 평균, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,5 +1,6 @@
 n = int(input())
 score = list(map(int, input().split()))
+max_score = max(score)
 
-modified_score = [i/max(score)*100 for i in score]
+modified_score = [i/max_score*100 for i in score]
 print(sum(modified_score)/n)
