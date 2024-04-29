8bbc796f219c6a3a80b0b22d39041e383525f851
Siyeon Won <96099990+1unasy@users.noreply.github.com>
2024-04-29 17:31:24 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,10 +1,10 @@
 n, m = map(int, input().split())
-basket = [0 for _ in range(n+1)]
+basket = [0 for _ in range(n)]
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    for i in range(a,b+1):
+    for i in range(a-1,b):
         basket[i] = c
 
-for i in basket[1:]:
+for i in basket:
     print(i, end=' ')
