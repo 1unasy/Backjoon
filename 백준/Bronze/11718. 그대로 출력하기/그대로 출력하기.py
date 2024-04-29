af47e9fcb4e256a76fb0763d65a891789c38a915
Siyeon Won <96099990+1unasy@users.noreply.github.com>
2024-04-29 22:18:03 +0900

    Update 그대로 출력하기.py

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -3,3 +3,14 @@ while True:
         print(input())
     except EOFError:
         break
+
+# 성능은 동일하게 48ms, 입력값 없을 때도 중지되도록 함
+#while True:
+#    try:
+#        word = input()
+#        if word != '':
+#            print(word)
+#        else:
+#            break
+#    except EOFError:
+#        break
